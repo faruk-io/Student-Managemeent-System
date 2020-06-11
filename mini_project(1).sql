3394efd137b573f5ef4acfabd27f795c1646b1d5
faruk-io <61500094+faruk-io@users.noreply.github.com>
2020-06-11 20:17:41 +0600

    Add files via upload

---
+++ mini_project(1).sql
@@ -0,0 +1,112 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 22, 2020 at 03:19 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.2.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mini_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_info`
+--
+
+CREATE TABLE `student_info` (
+  `id` int(5) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `roll` int(6) NOT NULL,
+  `class` varchar(3) NOT NULL,
+  `city` varchar(20) NOT NULL,
+  `pcontact` varchar(11) NOT NULL,
+  `photo` varchar(50) NOT NULL,
+  `datetime` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `student_info`
+--
+
+INSERT INTO `student_info` (`id`, `name`, `roll`, `class`, `city`, `pcontact`, `photo`, `datetime`) VALUES
+(17, '  kaniz munni153', 789754, '1st', '  kalihatitangail', '01977889233', '789754.JPG', '2020-03-10 18:20:16'),
+(18, 'Faruk Ahmed', 101010, '2nd', 'Tangail', '01719998721', '101010.png', '2020-03-16 17:25:34');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(5) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `photo` varchar(50) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `datetime` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `username`, `password`, `photo`, `status`, `datetime`) VALUES
+(3, 'test', 'test@gmail.com', 'test', '098f6bcd4621d373cade4e832627b4f6', 'test.JPG', 'active', '2020-03-15 17:11:56'),
+(9, 'farukahmedaraf', 'farukahmedaraf@gmail.com', 'farukahmedaraf', '31afb7e70a923c31d2acc0a54c921606', 'farukahmedaraf.JPG', 'active', '2020-03-16 17:19:15');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student_info`
+--
+ALTER TABLE `student_info`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `roll` (`roll`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `student_info`
+--
+ALTER TABLE `student_info`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
